769aa753930a19d2c8eff9665e2b789aa708a976
Rezadorost <rezadorost087@gmail.com>
2026-09-05 00:00:39 +0330

    feat: add reusable dependencies for name ID validation, fix (#1)

--- core/routes.py
+++ core/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from models import NameResponse , Name
 router = APIRouter()
 
@@ -9,17 +9,22 @@ names_list = {
     3:'Amir'
 }
 
+async def get_name_id(id: int):
+    return id
+
+async def check_name_id(id: int= Depends(get_name_id)):
+    if id not in names_list:
+        raise HTTPException(
+            status_code=404,
+            detail= "Not Found"
+        )
+    return id
 @router.get("/")
 async def root():
     return {'message' : 'hello from fast api'}
 
 @router.get("/names/{id}" , response_model= NameResponse)
-async def get_name(id : int):
-    if id not in names_list:
-        raise HTTPException(
-            status_code=404,
-            detail="Not Found"
-        )
+async def get_name(id : int= Depends(check_name_id)):
 
     return {
         'id': id ,
@@ -32,22 +37,11 @@ async def creat_name(new_name: Name):
    return names_list
 
 @router.delete("/names/{id}", status_code=204)
-async def delete_name(id : int):
-    if id not in names_list:
-        raise HTTPException(
-            status_code=404 ,
-            detail= "Not Found"
-        )
+async def delete_name(id : int= Depends(check_name_id)):
     del names_list[id]
     return names_list 
 
 @router.put("/names/{id}")
-async def put_name(id : int , name: str):
-    if id not in names_list:
-        raise HTTPException(
-            status_code=404 ,
-            detail= "Not Found"
-            )
-
+async def put_name(id : int, name: str= Depends(check_name_id)):
     names_list[id] = name
     return names_list
